1efd5c749489cf21b8382a9c5114ab59872014d6
Bryan Kok <bryan.wyern1@gmail.com>
2021-05-27 03:57:58 +0800

    Styling, use Glide to load the repo image into the RecyclerView rows

--- app/src/main/java/io/github/transfusion/nitroless/viewholders/NitrolessRepoViewHolder.kt
+++ app/src/main/java/io/github/transfusion/nitroless/viewholders/NitrolessRepoViewHolder.kt
@@ -2,7 +2,11 @@ package io.github.transfusion.nitroless.viewholders
 
 import android.util.Log
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
 import io.github.transfusion.nitroless.databinding.SourcesRowBinding
 import io.github.transfusion.nitroless.storage.NitrolessRepo
 
@@ -19,6 +23,18 @@ class NitrolessRepoViewHolder(private val binding: SourcesRowBinding) :
         binding.apply {
             repo = item
             executePendingBindings()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("imageUrl")
+        fun loadImage(
+            view: ShapeableImageView,
+            url: String?
+        ) { // This methods should not have any return type, = declaration would make it return that object declaration.
+            if (!url.isNullOrEmpty())
+                Glide.with(view.context).load(url).into(view)
         }
     }
 
